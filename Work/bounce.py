1688b1b48b5030b8b56f4b6f2b3868b8e410ede2
Joan Chen <joanchenuk@gmail.com>
2022-06-20 17:06:09 +0100

    task done

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
 # bounce.py
 #
 # Exercise 1.5
+'''
+Plan:
+input height: 100
+i = 10 
+h -> h * 3 / 5
+h = h * 3/5
+if h 
+'''
+
+def print_height(h, t):
+    for i in range(t):
+        h = h * 3 / 5
+        print(round(h, 4))
+
+print_height(100, 10)
+
+
